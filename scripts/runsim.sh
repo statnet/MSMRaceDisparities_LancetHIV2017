b6a285e31c22a65264812a47d52030c74fdbc94b
Sarah Stansfield <sestansfield@gmail.com>
2017-03-28 14:57:57 -0700

    Create runsim.sh

---
+++ scripts/runsim.sh
@@ -0,0 +1,22 @@
+### Example bash script to run
+
+#!/bin/bash
+
+## specifications
+#PBS -l nodes=1:ppn=16,mem=40gb,feature=16core
+#PBS -l walltime=180:00:00
+
+## Specify model name
+#PBS -N 04a.RaceAllEq.p05
+
+## Put the STDOUT and STDERR from jobs into the below directory in a single file 
+#PBS -o /gscratch/csde/goodreau/Mardham/04a.RaceAllEq.p05/
+#PBS -e /gscratch/csde/goodreau/Mardham/04a.RaceAllEq.p05/
+#PBS -j oe
+
+## Specify working directory
+#PBS -d /gscratch/csde/goodreau/Mardham/04a.RaceAllEq.p05/
+
+module load r_3.2.0
+
+R CMD BATCH --vanilla 03.sim.R 03.sim.Rout
